1828faa14e74b35661a29ea410fc5a6ec0f8c309
harjotdev <sharjot554.hs@gmail.com>
2026-01-06 15:20:07 +0530

    Initial Commit

---
+++ SwiftHome/SwiftHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftHomeApp.swift
+//  SwiftHome
+//
+//  Created by Harjot singh on 06/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
